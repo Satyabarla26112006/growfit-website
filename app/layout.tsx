65fc69d4d5964dd1e928b72cb377c11931e08587
Satyabarla26112006 <106729676+Satyabarla26112006@users.noreply.github.com>
2026-09-15 23:22:27 +0530

    Add AdSense verification code to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -37,15 +36,16 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        {children}
-
-        <Script
+      <head>
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1626383474633787"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
+      </head>
+
+      <body className="min-h-full flex flex-col">
+        {children}
       </body>
     </html>
   );
